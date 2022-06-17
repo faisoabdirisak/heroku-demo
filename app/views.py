c5298fcf6dcfeb18a511bd5e83d7a908adf01bae
faiso abdirisak <faisoabdirisak@gmail.com>
2022-06-17 09:12:57 +0300

    adding login restriction

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-from app.models import Activities, About, Contact, Sliders
+
 
 # Create your views here.
 
@@ -8,32 +9,27 @@ def home(request):
    
     return render(request, 'school/home.html')
 
-
+@login_required(login_url='login')
 def read(request):
     return render(request, 'school/read.html')
 
+@login_required(login_url='login')
 def math(request):
     return render(request, 'school/math.html')
 
-
+@login_required(login_url='login')
 def color(request):
     return render(request, 'school/color.html')
 
-
+@login_required(login_url='login')
 def environment(request):
     return render(request, 'school/environment.html')
 
+@login_required(login_url='login')
 def comp(request):
     return render(request, 'school/computer.html')
 
+@login_required(login_url='login')
 def religion(request):
     return render(request, 'school/religion.html')
-# def activities(request):
-#     activities=Activities.objects.all
-#     context={'activities':activities}
-#     return render(request, 'school/activities.html', context)
 
-# def activity(request,pk):
-#     activityObj=Activities.objects.get(id=pk)
-#     context={'activity':activityObj}
-#     return render(request, 'school/detail-activity.html', context)
