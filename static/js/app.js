10e8dd511469c0503c0255a1b26210bc38714dac
faiso abdirisak <faisoabdirisak@gmail.com>
2022-06-18 18:59:59 +0300

    add reviews and tutors

--- static/js/app.js
+++ static/js/app.js
@@ -171,3 +171,29 @@ function fadeOut(){
 }
 
 window.onload = fadeOut();
+
+
+
+// tutor
+
+
+// var swiper = new Swiper(".teachers-slider", {
+//   spaceBetween: 20,
+//   grabCursor:true,
+//   loop:true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   breakpoints: {
+//      540: {
+//        slidesPerView: 1,
+//      },
+//      768: {
+//        slidesPerView: 2,
+//      },
+//      1024: {
+//        slidesPerView: 3,
+//      },
+//   },
+// });
